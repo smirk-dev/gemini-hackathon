597229b2205bd659dc0ae6b41e215ad8cfe83837
smirk-dev <suryansh.mishra.sm6@gmail.com>
2026-02-04 22:33:17 +0530

    Handle ImportError for database_utils in utilities module

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,6 +1,10 @@
 """Utilities module initialization."""
 
-from .database_utils import get_connection
+try:
+    from .database_utils import get_connection
+except ImportError:
+    get_connection = None
+    
 from .thinking_log_viewer import render_thinking_log_viewer
 
 __all__ = [
